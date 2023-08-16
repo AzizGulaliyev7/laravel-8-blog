ea76b1c0e6c17bcedf8534d1f1ba174f6dfaf07a
aziz gulaliyev <azizgulaliyev44@gmil.com>
2023-08-16 22:03:13 +0500

    post from html

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,13 +5,7 @@
 
 <body>
     <article>
-        <h1>
-            My first post
-        </h1>
-
-        <p>
-            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
-        </p>
+        <?= $post ?>
     </article>
 
     <a href="/">Go back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 <body>
     <article>
         <h1>
-            <a href="/post">My first post</a>
+            <a href="/posts/my-first-post">My first post</a>
         </h1>
 
         <p>
@@ -16,7 +16,17 @@
 
     <article>
         <h1>
-            <a href="/post">My Second post</a>
+            <a href="/posts/my-second-post">My Second post</a>
+
+        </h1>
+
+        <p>
+            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/posts/my-third-post">My third post</a>
 
         </h1>
 
