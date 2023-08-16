c9a555c0529ba9c0781cdac5737e6be18c234801
aziz gulaliyev <azizgulaliyev44@gmil.com>
2023-08-16 21:39:27 +0500

    my first post

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>
+            <a href="/post">My first post</a>
+        </h1>
+
+        <p>
+            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post">My Second post</a>
+
+        </h1>
+
+        <p>
+            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
+        </p>
+    </article>
+</body>
