710d749488e8897551db9e74ad7c720bd3769923
aziz gulaliyev <azizgulaliyev44@gmil.com>
2023-08-17 22:16:04 +0500

    dynamic posts with collections

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,34 +4,18 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1>
-            <a href="/posts/my-first-post">My first post</a>
-        </h1>
 
-        <p>
-            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
 
-    <article>
-        <h1>
-            <a href="/posts/my-second-post">My Second post</a>
-
-        </h1>
-
-        <p>
-            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
-        </p>
-    </article>
-    <article>
-        <h1>
-            <a href="/posts/my-third-post">My third post</a>
-
-        </h1>
-
-        <p>
-            Please note that specific hotkeys and functionality might vary depending on your PHPStorm version, installed plugins, and custom keybindings. If the above steps do not work as expected, you can also consider creating a custom live template or snippet for generating Lorem Ipsum text.
-        </p>
-    </article>
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
+        </article>
+    <?php endforeach; ?>
 </body>
