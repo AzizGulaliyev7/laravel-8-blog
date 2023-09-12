b744aa8a2c32a17c4aacc0f5c783c32753349513
aziz gulaliyev <azizgulaliyev44@gmil.com>
2023-09-12 20:57:31 +0500

    mailchimp subscriber

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,8 +28,9 @@ class Post extends Model
         );
     }
 
-    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
-
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
     public function category() {
         return $this->belongsTo(Category::class);
     }
